a0a6c1221e16a4e52b1b812d0080cb71977f389d
sochubert <sochubert@gmail.com>
2022-11-29 20:10:11 +0900

    Setting Project Files

---
+++ TwitterTutorial/Controller/MainTabController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabController.swift
+//  TwitterTutorial
+//
+//  Created by Minjae Lee on 2022/11/29.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPurple
+    }
+}
