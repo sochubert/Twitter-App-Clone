9546e962d2cfbc9971b47e8a0a7ac09306cc4a51
sochubert <sochubert@gmail.com>
2022-12-01 00:06:57 +0900

    Login UI basic setting

---
+++ TwitterTutorial/Controller/Authentication/LoginController.swift
@@ -0,0 +1,42 @@
+//
+//  File.swift
+//  TwitterTutorial
+//
+//  Created by Minjae Lee on 2022/11/30.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+ 
+    //MARK: - Properties
+    
+    private let logoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.image = #imageLiteral(resourceName: "TwitterLogo")
+        return iv
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Selectors
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .twitterBlue
+        navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.isHidden = true
+        
+        view.addSubview(logoImageView)
+        logoImageView.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor)
+        logoImageView.setDimensions(width: 150, height: 150)
+    }
+}
---
+++ TwitterTutorial/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,28 @@
+//
+//  RegistrationController.swift
+//  TwitterTutorial
+//
+//  Created by Minjae Lee on 2022/11/30.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+ 
+    //MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Selectors
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .twitterBlue
+    }
+}
