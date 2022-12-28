634b3a016ae4c2a866f876b7b931fed29da836fc
sochubert <sochubert@gmail.com>
2022-12-28 10:39:59 +0900

    Tweet View Model

---
+++ TwitterTutorial/ViewModel/TweetViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  TweetViewModel.swift
+//  TwitterTutorial
+//
+//  Created by Minjae Lee on 2022/12/28.
+//
+
+import UIKit
+
+struct TweetViewModel {
+    let tweet: Tweet
+    let user: User
+    
+    var profileImageUrl: URL? {
+        return user.profileImageUrl
+    }
+    
+    var userInfoText: NSAttributedString {
+        let title = NSMutableAttributedString(string: user.fullname, attributes: [.font: UIFont.boldSystemFont(ofSize: 14)])
+        title.append(NSAttributedString(string: " @\(user.username)", attributes: [.font: UIFont.systemFont(ofSize: 14), .foregroundColor: UIColor.lightGray]))
+        
+        return title
+    }
+    
+    init(tweet: Tweet) {
+        self.tweet = tweet
+        self.user = tweet.user
+    }
+}
