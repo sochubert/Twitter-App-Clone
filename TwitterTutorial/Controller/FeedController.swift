ec18de29c873c765e454c9f7de00edfdc95b4be1
sochubert <sochubert@gmail.com>
2023-01-23 17:05:48 +0900

    Tweet Replies Setup

--- TwitterTutorial/Controller/FeedController.swift
+++ TwitterTutorial/Controller/FeedController.swift
@@ -104,6 +104,14 @@ extension FeedController: UICollectionViewDelegateFlowLayout {
 // MARK: -TweetDelegate
 
 extension FeedController: TweetCellDelegate {
+    func handleReplyTapped(_ cell: TweetCell) {
+        guard let tweet = cell.tweet else { return }
+        let controller = UploadTweetController(user: tweet.user, config: .reply(tweet))
+        let nav = UINavigationController(rootViewController: controller)
+        nav.modalPresentationStyle = .fullScreen
+        present(nav, animated: true, completion: nil)
+    }
+    
     func handleProfileImageTapped(_ cell: TweetCell) {
         guard let user = cell.tweet?.user else { return }
         let controller = ProfileController(user: user)
