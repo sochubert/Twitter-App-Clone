c1387c0722fa4aea51670aaf282c9cd6c8a54d7e
sochubert <sochubert@gmail.com>
2023-01-24 14:06:28 +0900

    Action Sheet setup

---
+++ TwitterTutorial/Utils/ActionSheetLauncher.swift
@@ -0,0 +1,25 @@
+//
+//  ActionSheetLauncher.swift
+//  TwitterTutorial
+//
+//  Created by Minjae Lee on 2023/01/24.
+//
+
+import Foundation
+
+class ActionSheetLauncher: NSObject {
+    
+    // MARK: - Properties
+    
+    private let user: User
+    init(user: User) {
+        self.user = user
+        super.init()
+    }
+    
+    // MARK: - Helpers
+    
+    func show() {
+        print("DEBUG: Show action sheet for user \(user.username)")
+    }
+}
--- TwitterTutorial/View/TweetHeader.swift
+++ TwitterTutorial/View/TweetHeader.swift
@@ -7,6 +7,10 @@
 
 import UIKit
 
+protocol TweetHeaderDelegate: class {
+    func showActionSheet()
+}
+
 class TweetHeader: UICollectionReusableView {
     
     // MARK: - Properties
@@ -14,6 +18,8 @@ class TweetHeader: UICollectionReusableView {
     var tweet: Tweet? {
         didSet { configure() }
     }
+    
+    weak var delegate: (TweetHeaderDelegate)?
     
     private lazy var profileImageView: UIImageView = {
         let iv = UIImageView()
@@ -169,7 +175,7 @@ class TweetHeader: UICollectionReusableView {
     }
     
     @objc func showActionSheet() {
-        
+        delegate?.showActionSheet()
     }
     
     @objc func handleCommentTapped() {
