de8da297314c27c1b9f5c1b5dce7eb648a688d37
sochubert <sochubert@gmail.com>
2023-01-26 22:04:16 +0900

    Fetch Notifications

--- TwitterTutorial/Controller/NotificationsController.swift
+++ TwitterTutorial/Controller/NotificationsController.swift
@@ -13,13 +13,24 @@ class NotificationsController: UITableViewController {
     
     // MARK: - Properties
     
-    private var notifications = [Notification]()
+    private var notifications = [Notification]() {
+        didSet { tableView.reloadData() }
+    }
     
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        fetchNotifications()
+    }
+    
+    // MARK: - API
+    
+    func fetchNotifications() {
+        NotificationService.shared.fetchNotifications { notifications in
+            self.notifications = notifications
+        }
     }
     
     // MARK: - Helpers
@@ -36,7 +47,7 @@ class NotificationsController: UITableViewController {
 
 extension NotificationsController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return notifications.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
